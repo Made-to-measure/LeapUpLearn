bf5d47bb8881c5b8f21180cf77fb7800710d003d
Jannik0210 <65861316+Jannik0210@users.noreply.github.com>
2021-04-23 15:56:02 +0200

    Update App.java
    test2

--- src/de/nrw/hspv/App.java
+++ src/de/nrw/hspv/App.java
@@ -12,7 +12,7 @@ public class App {
 	public static void main(String[] args) {
 		Mainframe mainframe = new Mainframe(); //erzeuge den Hauptscreen quasi Appstart
 		System.out.println("Test \t 123");
-
+		System.out.println("Test 2");
 	}
 
 }
