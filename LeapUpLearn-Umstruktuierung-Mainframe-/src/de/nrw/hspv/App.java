83deb4aacc3ab59b628a31d74320ff74292d8937
JanisBulmahn <81952864+JanisBulmahn@users.noreply.github.com>
2021-04-23 15:15:57 +0200

    Test Janis
    Test1

---
+++ LeapUpLearn-Umstruktuierung-Mainframe-/src/de/nrw/hspv/App.java
@@ -0,0 +1,18 @@
+package de.nrw.hspv;
+
+
+
+
+public class App {
+
+
+	/**
+	 * Die App starten
+	 */
+	public static void main(String[] args) {
+		Mainframe mainframe = new Mainframe(); //erzeuge den Hauptscreen quasi Appstart
+		
+
+	}
+
+}
