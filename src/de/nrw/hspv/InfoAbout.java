181bf84a78e9bf05e91c5e67e9a815e59444cd77
Jannik0210 <65861316+Jannik0210@users.noreply.github.com>
2021-04-23 13:35:45 +0200

    Add files via upload
    Ursprungsversion von Christian

---
+++ src/de/nrw/hspv/InfoAbout.java
@@ -0,0 +1,74 @@
+package de.nrw.hspv;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class InfoAbout extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			InfoAbout dialog = new InfoAbout();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setResizable(false);
+			dialog.setVisible(true);
+			dialog.setTitle("Über...");
+			dialog.setIconImage(Toolkit.getDefaultToolkit().getImage(App.class.getResource("/de/nrw/hspv/LUL.jpg")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public InfoAbout() {
+		setBounds(100, 100, 300, 200);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			JTextPane Info = new JTextPane();
+			String InfoText = "LeapUpLearn \n"
+					+ "Version 0.0.x \n"
+					+ "\n"
+					+ "Team: \n"
+					+ "Janis Bulmahn, Jannik Harmeling, Christian Koch ";
+			Info.setText(InfoText);
+			contentPanel.add(Info);
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.exit(EXIT_ON_CLOSE);
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+
+		}
+	}
+
+}
