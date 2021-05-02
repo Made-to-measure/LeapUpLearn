cbaa93930000957889260c727a14407418b340bf
JanisBulmahn <81952864+JanisBulmahn@users.noreply.github.com>
2021-05-02 11:31:00 +0200

    Update App.java

--- src/de/nrw/hspv/App.java
+++ src/de/nrw/hspv/App.java
@@ -10,7 +10,6 @@ public class App {
 	 * Die App starten
 	 */
 	public static void main(String[] args) {
-		LoginGUI loginGUI = new LoginGUI();		//erzeuge LoginGUi als Programmstart
 //		Mainframe mainframe = new Mainframe(); //erzeuge den Hauptscreen quasi Appstart
 	}
 
