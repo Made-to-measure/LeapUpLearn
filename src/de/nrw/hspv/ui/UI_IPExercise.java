e8dfca52604021f8048a3c53b20a91b1cabd675b
Jannik0210 <65861316+Jannik0210@users.noreply.github.com>
2021-05-11 11:43:56 +0200

    Update UI_IPExercise.java

--- src/de/nrw/hspv/ui/UI_IPExercise.java
+++ src/de/nrw/hspv/ui/UI_IPExercise.java
@@ -17,11 +17,12 @@ import javax.swing.border.LineBorder;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.logging.Level;
 import java.awt.event.ActionEvent;
 
 public class UI_IPExercise extends JPanel {
 	/**
-	 * Panel zeigt ein Beispiel f�r eine IP-Adressaufgabe an
+	 * Panel zeigt ein Beispiel fuer eine IP-Adressaufgabe an
 	 * 
 	 * @param
 	 * @author Jannik
@@ -32,6 +33,7 @@ public class UI_IPExercise extends JPanel {
 	//Erzeuge Frame
 	public UI_IPExercise() {
 		
+		
 		BorderLayout layout = new BorderLayout();
 		setLayout(layout);
 		
@@ -41,18 +43,20 @@ public class UI_IPExercise extends JPanel {
 //		flowLayout.setAlignment(FlowLayout.RIGHT);
 		add(fortfahren,BorderLayout.CENTER);
 		
+		//setze ButtonPanel unten rechts
 		JPanel buttonPanel = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) buttonPanel.getLayout();
 		flowLayout.setAlignment(FlowLayout.RIGHT);
 		fortfahren.add(buttonPanel, BorderLayout.SOUTH);
 		
+		//erzeuge Fortfahren Button
 		JButton fortButton = new JButton("Fortfahren");
 		fortButton.setHorizontalAlignment(SwingConstants.RIGHT);
 		fortButton.setAlignmentX(RIGHT_ALIGNMENT);
 		fortButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				remove(fortfahren);
-				add(new UI_IPRuntime(), BorderLayout.CENTER);
+				add(new UI_IPRuntime(), BorderLayout.CENTER); //Erzeuge UI_UPRuntime im Center des Panels
 				revalidate();
 				repaint();
 			}
@@ -65,9 +69,11 @@ public class UI_IPExercise extends JPanel {
 	}
 	
 	class Beschreibung extends JPanel{
-		
+		/**
+		 * Klasse ist ein Panel, welches Hinweise für die Bearbeitung anzeigt und ein Beispielbild einfügt
+		 */
 		Beschreibung(){
-			
+			//setze Layout
 			GridBagLayout gridBagLayout = new GridBagLayout();
 			gridBagLayout.columnWidths = new int[]{409, 0};
 			gridBagLayout.rowHeights = new int[]{14, 14, 14, 0, 0};
@@ -75,6 +81,7 @@ public class UI_IPExercise extends JPanel {
 			gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 			setLayout(gridBagLayout);
 			
+			//setze erstes Label mit Hinweis
 			JLabel lblNewLabel = new JLabel("In diesem Teil k\u00F6nnen Sie f\u00FCr ihre Pr\u00FCfung im Teilmodul Grld-IT \u00FCben.");
 			lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
 			GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
@@ -84,6 +91,7 @@ public class UI_IPExercise extends JPanel {
 			gbc_lblNewLabel.gridy = 0;
 			add(lblNewLabel, gbc_lblNewLabel);
 			
+			//setze zweites Label mit Hinweis
 			JLabel lblNewLabel_1 = new JLabel("Bitte nutzen Sie f\u00FCr die Eingabe der IP-Adressen folgendes Format: xxx.xxx.xxx.xxx");
 			lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
 			GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
@@ -102,8 +110,7 @@ public class UI_IPExercise extends JPanel {
 			gbc_lblNewLabel_2.gridy = 2;
 			add(lblNewLabel_2, gbc_lblNewLabel_2);
 			
-			
-			
+			//Füge Panel ein in dem das Bespielbild angezeigt werden soll
 			JPanel imagePanel = new JPanel();
 			FlowLayout flowLayout = (FlowLayout) imagePanel.getLayout();
 			flowLayout.setAlignment(FlowLayout.LEFT);
@@ -113,8 +120,9 @@ public class UI_IPExercise extends JPanel {
 			gbc_panel.gridx = 0;
 			gbc_panel.gridy = 3;
 			add(imagePanel, gbc_panel);
-
+			
 			try {
+				//Fuege Bild ein
 				Image exampleImage = Toolkit.getDefaultToolkit().getImage(App.class.getResource("/de/nrw/hspv/example.jpg"));
 				JLabel iconLabel = new JLabel(new ImageIcon(exampleImage));
 				imagePanel.add(iconLabel);
@@ -123,13 +131,10 @@ public class UI_IPExercise extends JPanel {
 				iconLabel.setBorder(new CompoundBorder(border, margin));
 			}
 			catch (Exception e){
-				e.printStackTrace();
+				App.logger.log(Level.SEVERE, "Fehler beim Laden des Beispielbilds", e);
 			}
 			
-		}
-	
-	
-	
+		}	
 	}
 }
 
